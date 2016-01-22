1c8d11dfa99d128b18374e581ecde814dbabb106
strivegfx <notifydevon@gmail.com>
2016-01-22 20:11:05 +1300

    Setting up a standard webpack build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    context: __dirname + '/src/js',
+    devtool: 'source-map',
+    entry: './entry.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        // new CopyWebpackPlugin([
+        //     { from:  '../index.html', to: 'index.html' },
+        //     { from:  '../img', to: 'img' }
+        // ]),
+        new ExtractTextPlugin('style.css')
+    ],
+    module: {
+        loaders: [
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap!autoprefixer?browsers=last 2 versions!sass') },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel!eslint'} // Automatically generates source maps without the sourceMaps config
+        ]
+    },
+    eslint: {
+        failOnWarning: false,
+        failOnError: false
+    }
+};
