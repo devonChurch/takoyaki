9fac980a68803ec72d7c65e9303b3c18ba4ffa50
strivegfx <notifydevon@gmail.com>
2016-02-13 17:27:38 +1300

    Added in the function to generate a new post via NPM

--- build/home/curate.js
+++ build/home/curate.js
@@ -1,6 +1,77 @@
 'use strict';
 
+const fs = require('fs');
 const posts = require('../posts/order');
-const extract = require('./extract');
+const dir = '../../src/';
 
-extract(posts);
+{
+
+    let snippets = compileSnippets();
+    snippets = constructSnippets(snippets);
+    injectSnippets(snippets);
+
+    console.log('SUCCESS: Curated post onto the home page as the appear in the build/posts/order.js config file');
+
+}
+
+function compileSnippets() {
+
+    let snippets = '';
+
+    for (const post of posts) {
+
+        let data = getPost(post);
+        data = distillData(post, data);
+        snippets += data;
+
+    }
+
+    return snippets;
+
+}
+
+function getPost(post) {
+
+    return fs.readFileSync(`${dir}posts/${post}.jade`, 'utf8');
+
+}
+
+function distillData(post, data) {
+
+    return `
+    {
+        href: '${post}',
+        ${extractData(data)}
+    },
+`;
+
+}
+
+function extractData(data) {
+
+    const open = 'page = {';
+    const close = '} //- page ]';
+    const start = data.indexOf(open) + open.length;
+    data = data.substr(start);
+    const end = data.indexOf(close);
+
+    return data.substr(0, end).trim();
+
+}
+
+function constructSnippets(snippets) {
+
+    return `
+-
+    snippets = [
+        ${snippets}
+    ];
+`;
+
+}
+
+function injectSnippets(snippets) {
+
+    fs.writeFileSync(`${dir}partials/curated.jade`, snippets);
+
+}
